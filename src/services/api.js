6d9c9b4e176d17d757abc55cbae0ee3ec91ead64
KSArjun18 <arjunkishore18@gmail.com>
2025-09-15 03:23:40 +0530

    render url added

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_BASE_URL = "https://rag-backend-p744.onrender.com"
 
 export const createSession = async () => {
   const response = await axios.post(`${API_BASE_URL}/session`);
